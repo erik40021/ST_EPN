056e675c7bd37386a9fb69bbef3029f02ee24f0b
erik40021 <32685416+erik40021@users.noreply.github.com>
2025-05-14 12:10:01 +0200

    Update NMF_utils.R

--- utils/NMF_utils.R
+++ utils/NMF_utils.R
@@ -1,3 +1,29 @@
+run_NMF = function(sobj_path, out_path, range, n_features) {
+  message("Running NMF for sample ", s)
+  out_dir = file.path(out_path, s); if (!dir.exists(out_dir)) { dir.create(out_dir, r = T) }
+  
+  # 1. ---- load and prepare data -----
+  sobj = readRDS(file = paste0(sobj_path, "/sobj", s, ".rds"))
+  data = as.matrix(GetAssayData(sobj, layer = 'scale.data'))
+  data = data[VariableFeatures(sobj)[1:n_features], ]
+  data[data < 0] = 0  # set negative values to zero
+  data = data[apply(data, 1, var) > 0,] # subset on genes with a variance above zero
+  message("[", s, "] Extracted matrix of dimensions ", dim(data)[1], " (genes) x ", dim(data)[2], " (spots)")
+  
+  # 2. ---- run NMF -----
+  message("running NMF for ranks ", range[1], "-", tail(range, 1), " | start time: ", format(Sys.time(), "%d.%m. %X"))
+  res.list = lapply(range, function(r) {
+      message(r, " (", format(Sys.time(), "%X"), ")")
+      nmf(data, rank = r, nrun = 1, seed = "ica", method = "nsNMF")
+  })
+  names(res.list) = range
+  saveRDS(res.list, file = paste0(out_dir, "/raw_res-list_", s, ".rds"))
+}
+
+
+
+
+
 plot_NMF_heatmap = function(data, xlab = NULL, ylab = NULL, limits = c(2, 25), cols = custom_magma) {
   midpoint = limits[1] + (limits[2] - limits[1]) / 2
   p = ggplot(data, aes(x=Var1, y=Var2, fill=100*value/(100-value), color=100*value/(100-value))) + geom_tile() + 
